7387c439ae623124938abdab685cb874d8060177
Wallace Goulart Gaudie Ley <wggley1981-github@yahoo.com.br>
2017-10-04 18:29:51 -0300

    Alteracoes para funcionar modulo

--- MobiForms/app/app.module.js
+++ MobiForms/app/app.module.js
@@ -11,9 +11,32 @@
         .module('pogo.mobiforms.web', injection)
         .config(['coreConfig', 'mobiFormsConfig', function(coreConfig, mobiFormsConfig) {
 
-        }])
-        .run([function() {
-            //After Login State
-
         }]);
+
+
+
+    angular
+        .module('pogo.mobiforms.web')
+        .run(run);
+
+    run.$inject = ['$ocLazyLoad', 'mobiFormsConfig', 'coreConfig', '$translate'];
+
+    /**
+     * Run function
+     */
+    function run($ocLazyLoad, mobiFormsConfig, coreConfig, $translate) {
+        var moduleFilesOnSerie = [
+            mobiFormsConfig.directories.assets + '/css/styles.css',
+            mobiFormsConfig.directories.app + '/app.states.js',
+            mobiFormsConfig.directories.app + '/app.translate.js',
+        ];
+
+        $ocLazyLoad.load([moduleFilesOnSerie], {
+            serie: true,
+        }).then(function() {
+            $translate.refresh();
+        });
+        //After Login State
+
+    }
 })();
